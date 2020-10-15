f3770d58d16413ba6ef0cbcc9ee1c0683ac19016
Courtney Jacobs <cjacobs124@gmail.com>
2020-10-15 16:32:12 -0400

    Ready to add email

--- portfolioPages/views.py
+++ portfolioPages/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 #HomePage
 def home(request):
-    return render(request, "home.html", {})
+    if request.method == "POST":
+        message_name = request.POST['name']
+        message_email = request.POST['email']
+        message = request.POST['message']
+
+        return render(request, 'home.html', {'message_name': message_name, 'message_email': message_email, 'message': message})
+    
+    else:
+        # return the page
+
+        return render(request, "home.html", {})
+
+#ContactPage
+def contact(request):
+
+        return render(request, 'contact.html', {})
