b9ee9fc7832f2dc6ceda823ddf3823212d989b59
Courtney Jacobs <cjacobs124@gmail.com>
2020-10-15 17:10:00 -0400

    email started

--- portfolioHQ/settings.py
+++ portfolioHQ/settings.py
@@ -117,6 +117,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'csoc783@gmail.com'
+EMAIL_HOST_PASSWORD = 'setuP984@2%'  # use environmental variable
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 STATICFILES_STORAGE = 'whitenoise.storage. CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
--- portfolioPages/views.py
+++ portfolioPages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 #HomePage
 def home(request):
@@ -6,6 +7,15 @@ def home(request):
         message_name = request.POST['name']
         message_email = request.POST['email']
         message = request.POST['message']
+
+        # Send email
+        send_mail(
+            "Message from:" + message_name, # Subject
+            message_email, # email
+            message, # message
+            ['courtneyj3470@gmail.com'], # To email
+            fail_silently=False, # Fail Safe
+        )
 
         return render(request, 'home.html', {'message_name': message_name, 'message_email': message_email, 'message': message})
     
